53fdb807e71994a47602f24eb1026970c6672564
Rafael Aldana <rafaelap@kth.se>
2016-12-05 21:51:26 +0100

    added overviewActivity

---
+++ MySL/app/src/main/java/se/kth/ict/mysl/OverviewActivity.java
@@ -0,0 +1,17 @@
+package se.kth.ict.mysl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RafaelA on 2016-12-05.
+ */
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overview);
+    }
+}
