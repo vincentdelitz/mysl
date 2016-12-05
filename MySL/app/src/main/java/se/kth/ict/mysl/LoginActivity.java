840c82fabd21dc464747cbc952c79e04f5c1df8e
Rafael Aldana <rafaelap@kth.se>
2016-12-05 22:39:00 +0100

    added login

---
+++ MySL/app/src/main/java/se/kth/ict/mysl/LoginActivity.java
@@ -0,0 +1,18 @@
+package se.kth.ict.mysl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RafaelA on 2016-12-05.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
+
