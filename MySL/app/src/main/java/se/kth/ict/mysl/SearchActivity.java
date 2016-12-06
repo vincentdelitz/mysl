62bfa633c6a68425fe1b061c0ee39d6fa17aa8c2
Vincent Delitz <vincent.delitz@tum.de>
2016-12-06 10:58:14 +0100

    minor adjustments

--- MySL/app/src/main/java/se/kth/ict/mysl/SearchActivity.java
+++ MySL/app/src/main/java/se/kth/ict/mysl/SearchActivity.java
@@ -1,8 +1,10 @@
 package se.kth.ict.mysl;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class SearchActivity extends AppCompatActivity {
@@ -11,10 +13,23 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void openSpecifySearch(View view) {
         Intent intent = new Intent(this, SpecifySearch.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
