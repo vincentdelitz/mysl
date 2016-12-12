90815270fd8ada693aa287fc14a43d34c331af34
NOVATOR\RafaelA <rafael.aldana@novator.eu>
2016-12-12 21:00:02 +0100

    Created a Database Using a SQL Helper

--- MySL/app/src/main/java/se/kth/ict/mysl/FeedReaderContract.java
+++ MySL/app/src/main/java/se/kth/ict/mysl/FeedReaderContract.java
@@ -7,17 +7,6 @@ import android.provider.BaseColumns;
  */
 
 public final class FeedReaderContract {
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String COMMA_SEP = ",";
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_SUBTITLE + TEXT_TYPE + " )";
-
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
-
     // To prevent someone from accidentally instantiating the contract class,
     // make the constructor private.
     private FeedReaderContract() {
